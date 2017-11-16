e96337a8028fb87f49ddd37f6e517e9074dd2280
alfjuniorbh <alfjuniorbh.web@gmail.com>
2017-11-16 17:26:56 -0200

    Video

--- resources/views/site/layout/banner.blade.php
+++ resources/views/site/layout/banner.blade.php
@@ -7,7 +7,7 @@
             <div class="col-md-12 text-center fh5co-heading">
                 <p>Período da campanha : 15/11/17 a 18/2/18</p>
                 <div class="videoWrapper">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/ZUtmFrD7mlQ?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/mRF0fzudlLU?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
                 </div>
                 <h3>Acelere sua sorte! Pague tudo com o Sicoobcard* <br/>e concorra a um carro 0km.</h3>
                 <h4>No débito ou crédito, quanto mais você usar, mais chances tem de ganhar.</h4>
