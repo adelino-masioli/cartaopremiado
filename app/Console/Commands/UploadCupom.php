5d376df704067c4f21f4d033dbe9b5594c84c17c
alfjuniorbh <alfjuniorbh.web@gmail.com>
2017-12-12 20:54:13 -0400

    Table cupons

---
+++ app/Console/Commands/UploadCupom.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\Cupom;
+use League\Csv\Reader;
+
+class UploadCupom extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:uploadcupom';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Uploads Cupons';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        /*-------------*/
+        $arquivo = public_path() . '/' .'upload/CAMPANHA_11_UAR1_CUPONS_ATRIB_20171204.csv';
+        if (!empty($arquivo)) {
+
+            //$filename = public_path() . '/' . $arquivo;
+            $filename =  $arquivo;
+            if ($filename) {
+
+                $csv = Reader::createFromPath($filename);
+
+                //seta o delimitador
+                $csv->setDelimiter(";");
+
+                foreach ($csv as $index => $row) {
+                    if ($row[0] != '') {
+                        if ($index > 0) {
+                            $document1 = str_replace('-', '', trim($row[1]));
+                            $document2 = str_replace('.', '', $document1);
+                            $cupom     = str_replace(',', '', trim($row[3]));
+                            $locale_name = str_replace('REGIAO', '', trim($row[4]));
+
+                            $data_db = array(
+                                'document'   => trim($document2),
+                                'serie'      => trim($row[2]),
+                                'cupom'      => trim($cupom),
+                                'uar'        => trim($row[4]),
+                                'singular'   => trim('Região '.$locale_name),
+                                'created_at' => date('Y-m-d H:i:s'),
+                                'updated_at' => date('Y-m-d H:i:s'),
+
+                            );
+                            $validate = Cupom::where('cupom', $cupom)->count();
+                            if($validate == 0) {
+                                $createcsv = new Cupom($data_db);
+                                $createcsv->save();
+                            }
+                        }
+                    }
+                }
+
+            } else {
+                return FALSE;
+            }
+
+        }
+        /*-------------*/
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\UploadCupom::class,
     ];
 
     /**
@@ -26,6 +26,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+
+        $schedule->command('command:uploadcupom')->daily();
     }
 
     /**
@@ -36,7 +38,7 @@ class Kernel extends ConsoleKernel
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
-
         require base_path('routes/console.php');
+        
     }
 }
