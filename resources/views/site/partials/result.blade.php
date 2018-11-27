988c5ebcc740b3214b9ed0181f40889c8e5389a4
Adelino Junior <alfjuniorbh.web@gmail.com>
2018-11-27 15:42:59 -0200

    Table Cooperativa

--- resources/views/site/partials/result.blade.php
+++ resources/views/site/partials/result.blade.php
@@ -7,57 +7,70 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-12 text-center animate-box">
+            <div class="col-xs-12 col-md-10 col-md-offset-1 text-center animate-box">
                 <div class="results">
                     <div class="desc">
-                        <h3>Sorteio Mensal - 23/01/2019 | 01 (um) Renaut Kwind Zen 0Km</h3>
+                        <h3>Sorteio Mensal - 23/01/2019 | 01 (um) Renault Kwid Zen 0Km</h3>
                     </div>
                     <ul class="list-results-head">
-                        <li class="col-md-5">Nome:</li>                        
+                        <li class="col-md-5">Nome:</li> 
+                        <li class="col-md-4">Número da Sorte:</li>                       
                         <li class="col-md-3">Cooperativa:</li>
-                        <li class="col-md-1">PA:</li>
-                        <li class="col-md-3">Cidade:</li>
                     </ul>
 
                     <ul class="list-results-content">
-                        <li class="col-md-5"><div class="first">                    <span>Nome do ganhador aqui</span></div></li>
-                        <li class="col-md-3"><div class="second">                   <span>Nome da Cooperativa</span></div></li>
-                        <li class="col-md-1"><div class="second" style="width:84%;"><span class="text-center">00</span></div></li>
-                        <li class="col-md-3"><div class="second">                   <span>Cidade</span></div></li>
+                        <li class="col-md-5"><div class="first">  <span>Nome do ganhador aqui</span></div></li>
+                        <li class="col-md-4"><div class="second"> <span>Número 123.567.79-10</span></div></li>
+                        <li class="col-md-3"><div class="second"> <span>&nbsp;</span></div></li>                      
                     </ul>
 
-                    <div class="desc">
-                        <h3>Sorteio Mensal - 20/02/2019 | 01 (um) Renaut Kwind Zen 0Km</h3>
+                    <div class="desc" style="padding-top:80px;">
+                        <h3>Sorteio Mensal - 20/02/2019 | 01 (um) Renault Kwid Zen 0Km</h3>
                     </div>
                     <ul class="list-results-head">
-                        <li class="col-md-5">Nome:</li>                        
+                        <li class="col-md-5">Nome:</li> 
+                        <li class="col-md-4">Número da Sorte:</li>                       
                         <li class="col-md-3">Cooperativa:</li>
-                        <li class="col-md-1">PA:</li>
-                        <li class="col-md-3">Cidade:</li>
                     </ul>
 
                     <ul class="list-results-content">
-                        <li class="col-md-5"><div class="first">                    <span>Nome do ganhador aqui</span></div></li>
-                        <li class="col-md-3"><div class="second">                   <span>Nome da Cooperativa</span></div></li>
-                        <li class="col-md-1"><div class="second" style="width:84%;"><span class="text-center">00</span></div></li>
-                        <li class="col-md-3"><div class="second">                   <span>Cidade</span></div></li>
+                        <li class="col-md-5"><div class="first">  <span>Nome do ganhador aqui</span></div></li>
+                        <li class="col-md-4"><div class="second"> <span>Número 123.567.79-10</span></div></li>
+                        <li class="col-md-3"><div class="second"> <span>&nbsp;</span></div></li>                      
                     </ul>
 
-                    <div class="desc">
-                        <h3>Sorteio Mensal - 20/02/2019 | 09 (nove) Renaut Kwind Zen 0Km</h3>
+                    <div class="desc" style="padding-top:80px;">
+                        <h3>Sorteio Regional - 20/03/2019 | 09 (nove) Renaults Kwid Zen 0Km</h3>
                     </div>
                     <ul class="list-results-head">
-                        <li class="col-md-5">Nome:</li>                        
+                        <li class="col-md-5">Nome:</li> 
+                        <li class="col-md-4">Número da Sorte:</li>                       
                         <li class="col-md-3">Cooperativa:</li>
-                        <li class="col-md-1">PA:</li>
-                        <li class="col-md-3">Cidade:</li>
+                    </ul>
+
+
+                    @for($i=0;$i<9;$i++)
+                    <ul class="list-results-content">
+                        <li class="col-md-5"><div class="first">  <span>Nome do ganhador aqui</span></div></li>
+                        <li class="col-md-4"><div class="second"> <span>Número 123.567.79-10</span></div></li>
+                        <li class="col-md-3"><div class="second"> <span>&nbsp;</span></div></li>                      
+                    </ul>
+                    @endfor
+
+
+                    <div class="desc" style="padding-top:80px;">
+                        <h3>Sorteio Final - 27/03/2019 | 01 (uma) Toyota Hilux 4x2 Flex</h3>
+                    </div>
+                    <ul class="list-results-head">
+                        <li class="col-md-5">Nome:</li> 
+                        <li class="col-md-4">Número da Sorte:</li>                       
+                        <li class="col-md-3">Cooperativa:</li>
                     </ul>
 
                     <ul class="list-results-content">
-                        <li class="col-md-5"><div class="first">                    <span>Nome do ganhador aqui</span></div></li>
-                        <li class="col-md-3"><div class="second">                   <span>Nome da Cooperativa</span></div></li>
-                        <li class="col-md-1"><div class="second" style="width:84%;"><span class="text-center">00</span></div></li>
-                        <li class="col-md-3"><div class="second">                   <span>Cidade</span></div></li>
+                        <li class="col-md-5"><div class="first">  <span>Nome do ganhador aqui</span></div></li>
+                        <li class="col-md-4"><div class="second"> <span>Número 123.567.79-10</span></div></li>
+                        <li class="col-md-3"><div class="second"> <span>&nbsp;</span></div></li>                      
                     </ul>
                      
                 </div>
@@ -68,7 +81,7 @@
             <div class="col-md-12 text-center animate-box">
                 <div class="results">
                     <div class="desc">
-                        <h3>Sorteio Mensal - 23/01/2019 | 01 (um) Renaut Kwind Zen 0Km</h3>
+                        <h3>Sorteio Mensal - 23/01/2019 | 01 (uma) Toyota Hilux 4x2 Flex</h3>
                         <h3>O sorteio ainda não foi realizado.</h3>
                     </div>
                 </div>
